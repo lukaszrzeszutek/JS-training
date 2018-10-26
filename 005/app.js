929c7f7aa43546416483806d60b38fe5d970c18b
lukaszrzeszutek <lucas.rzeszut@gmail.com>
2018-10-26 23:43:24 +0200

    console

---
+++ 005/app.js
@@ -0,0 +1,31 @@
+console.log('Hey');
+console.log(123);
+console.log(true);
+
+// variable
+var greeting = 'Hello friend';
+console.log(greeting);
+
+// array
+console.log([1,2,3,4]);
+// objects
+console.log({a:1, b:2});
+console.table({a:1, b:2});
+
+// Errors
+console.error('This is some error');
+
+// Clear console
+console.clear();
+
+// warnings
+console.warn('This is a warning');
+
+//time operation
+console.time('Hello');
+  console.log('Hey');
+  console.log('Hey');
+  console.log('Hey');
+  console.log('Hey');
+  console.log('Hey');
+console.timeEnd('Hello');
