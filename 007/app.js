ffb405541edc595388b13813d115abbdc2171dbb
lukaszrzeszutek <lucas.rzeszut@gmail.com>
2018-10-30 23:17:01 +0100

    data types in JS

---
+++ 007/app.js
@@ -0,0 +1,33 @@
+// PRIMITIVE TYPES
+// String
+const name = 'John Cena';
+
+// Numbers
+const age = 24;
+
+// Boolean
+const hasKids = true;
+
+// Null
+const car = null;
+
+// Undefined
+let test;
+
+// Symbol
+const sym = Symbol();
+
+
+// REFERENCE TYPES - OBJECTS
+// Array
+const hobbies = ['movies', 'music'];
+
+// Object literal
+const address = {
+  city: 'Boston',
+  state: 'MA'
+}
+
+const today = new Date();
+console.log(today);
+console.log(typeof today); // (typeof zwraca typ zmiennej)
