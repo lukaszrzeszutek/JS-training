2487b8e7266f561dd83ef56ba21dbd278eae0032
lukaszrzeszutek <lucas.rzeszut@gmail.com>
2018-11-01 22:21:11 +0100

    numbers the math object

---
+++ 009/app.js
@@ -0,0 +1,26 @@
+const num1 = 100;
+const num2 = 30;
+let val;
+
+// Simple math with numbers
+val = num1 + num2;
+val = num1 - num2;
+val = num1 * num2;
+val = num1 / num2;
+val = num1 % num2;
+
+// Math Objects
+val = Math.PI;
+val = Math.E;
+val = Math.round(2.3);
+val = Math.ceil(2.3);
+val = Math.floor(2.8);
+val = Math.sqrt(64);
+val = Math.abs(-3);
+val = Math.pow(8, 2);
+val = Math.min(2,33,44,-2,-5,1,0);
+val = Math.max(2,33,44,-2,-5,1,0);
+val = Math.random();
+val = Math.floor(Math.random() *20);    //random number 1 to 20
+
+console.log(val);
